18ded500d17e853d4a3c3891373ec88754c2e28c
Yujia Lian <yujialia@buffalo.edu>
2018-07-21 16:17:09 -0400

    Add selection sort.

--- Sorting/selectionSort.java
+++ Sorting/selectionSort.java
@@ -11,6 +11,11 @@ public class selectionSort {
             swap(arr, min, i);
         }
     }
+    void swap(int[] arr, int i, int j) {
+        int tmp = arr[i];
+        arr[i] = arr[j];
+        arr[j] = tmp;
+    }
     public static void main(String[] args) {
         selectionSort test = new selectionSort();
         int[] arr = new int[] {5,7,1,3,7,9,5,2,6};
@@ -18,11 +23,6 @@ public class selectionSort {
         for(int k: arr) {
             System.out.println(k);
         }
-    }
-    void swap(int[] arr, int i, int j) {
-        int tmp = arr[i];
-        arr[i] = arr[j];
-        arr[j] = tmp;
     }
 }
 
