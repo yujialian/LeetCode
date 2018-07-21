c7062427b00795831d057b8109041d615a1a2aa9
Yujia Lian <yujialia@buffalo.edu>
2018-07-21 15:55:19 -0400

    Add selection sort and 3 colors quicksort 3 way solution.

---
+++ Sorting/selectionSort.java
@@ -0,0 +1,31 @@
+public class selectionSort {
+    void selection(int[] arr) {
+        if(arr == null || arr.length == 0) {
+            return;
+        }
+        for(int i = 0; i < arr.length; i++) {
+            int min = i;
+            for(int j = i + 1; j < arr.length; j++) {
+                if(arr[j] < arr[min]) min = j;
+            }
+            swap(arr, min, i);
+        }
+    }
+    public static void main(String[] args) {
+        selectionSort test = new selectionSort();
+        int[] arr = new int[] {5,7,1,3,7,9,5,2,6};
+        test.selection(arr);
+        for(int k: arr) {
+            System.out.println(k);
+        }
+    }
+    void swap(int[] arr, int i, int j) {
+        int tmp = arr[i];
+        arr[i] = arr[j];
+        arr[j] = tmp;
+    }
+}
+
+
+
+
