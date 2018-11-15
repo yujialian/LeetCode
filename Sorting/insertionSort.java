db39a31cf1a17502fdce1931f927d36e803f9e10
yujialia <yujialia@buffalo.edu>
2018-11-15 14:33:22 -0500

    Add insertion and selection sort. Refresh memories.

--- Sorting/insertionSort.java
+++ Sorting/insertionSort.java
@@ -1,23 +1,21 @@
-public class insertionSort {
-    void insertionSort(int[] arr) {
-        for(int i = 1; i < arr.length; i++) {
+public class insertionSort<E extends Comparable<E>> {
+    public void insertionSort(E[] arr) {
+        for(int i = 0; i < arr.length; i++) {
             for(int j = i; j > 0; j--) {
-                if(arr[j] < arr[j-1]) {
-                    swap(arr, j, j-1);
-                } else {
-                    break;
+                if(arr[j].compareTo(arr[j - 1]) < 0) {
+                    swap(j, j - 1, arr);
                 }
             }
         }
     }
-    void swap(int[] arr, int i, int j) {
-        int tmp = arr[i];
+    private void swap(int i, int j, E[] arr) {
+        E tmp = arr[i];
         arr[i] = arr[j];
         arr[j] = tmp;
     }
     public static void main(String[] args) {
-        insertionSort test = new insertionSort();
-        int[] arr = new int[] {5,7,1,3,7,9,5,2,6};
+        insertionSort<Integer> test = new insertionSort<>();
+        Integer[] arr = new Integer[] {5,7,1,3,7,9,5,2,6};
         test.insertionSort(arr);
         for(int k: arr) {
             System.out.println(k);
--- Sorting/selectionSort.java
+++ Sorting/selectionSort.java
@@ -1,24 +1,22 @@
-public class selectionSort {
-    void selection(int[] arr) {
-        if(arr == null || arr.length == 0) {
-            return;
-        }
+public class selectionSort<E extends Comparable<E>> {
+    public void selection(E[] arr) {
+        if(arr == null || arr.length == 0) return;
         for(int i = 0; i < arr.length; i++) {
-            int min = i;
+            int minIndex = i;
             for(int j = i + 1; j < arr.length; j++) {
-                if(arr[j] < arr[min]) min = j;
+                if(arr[j].compareTo(arr[minIndex]) < 0) minIndex = j;
             }
-            swap(arr, min, i);
+            if(i != minIndex) swap(arr, minIndex, i);
         }
     }
-    void swap(int[] arr, int i, int j) {
-        int tmp = arr[i];
+    private void swap(E[] arr, int i, int j) {
+        E tmp = arr[i];
         arr[i] = arr[j];
         arr[j] = tmp;
     }
     public static void main(String[] args) {
-        selectionSort test = new selectionSort();
-        int[] arr = new int[] {5,7,1,3,7,9,5,2,6};
+        selectionSort<Integer> test = new selectionSort<>();
+        Integer[] arr = new Integer[] {5,7,1,3,7,9,5,2,6};
         test.selection(arr);
         for(int k: arr) {
             System.out.println(k);
